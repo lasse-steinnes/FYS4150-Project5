ee06e1012991147064df72cf8e9cec424191814b
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-11-30 22:47:49 +0100

    explicit euler

--- code-and-results/explicit_euler.cpp
+++ code-and-results/explicit_euler.cpp
@@ -3,14 +3,34 @@
 #include <iostream>
 #include <chrono>
 
-void Explicit_Euler::init(){
-
-} // if an init needed here
-
-void Explicit_Euler::advance(){
-
+void Explicit_Euler::init(double T, double dt, int Lx, double dx){
+  initialize(T, dt, Lx, dx);
 }
 
-void Explicit_Euler::solve(){
-  
+void Explicit_Euler::set_initial(double I(double x)){  // set up the inital condition
+  // Note: x[i-Ix[0]] Is the right index.
+  for (int i = 1; i < m_Nx; i++){ //Ix.back gets last element in vector
+              u_n(i) = I(m_x(i));
+    };
+
+  // enforcing dirichlet boundary conditions
+  u_n(0) = 0; u_n(m_Nx) = 0;
+}
+
+void Explicit_Euler::advance(){
+  // advance in space
+  for (int i = 1; i < m_Nx;i++){
+    u(i) = u_n(i) + (m_dt/m_dx2)*(u_n(i+1) - 2*u_n(i) + u_n(i-1));
+    }
+
+    // Enforce Dirichlet boundary condition
+    u(0) = 0; u(m_Nx) =  0;
+    u_n = u; // Set u_m = u to be used in next time step (copy u over to u_n)
+}
+
+void Explicit_Euler::solve(){ // solves the system in time
+  // Uses advance to solve the system for inner mesh points in time
+  for (int n = 0; n < m_Nt;n++){
+      advance();
+  }
 }
--- code-and-results/finitediffs.cpp
+++ code-and-results/finitediffs.cpp
@@ -1,7 +1,38 @@
 // superclass methods
 #include "finitediffs.hpp"
+#include <cmath>
+#include <armadillo>
 
-void Finite_Difference::initialize(){
-  
+void Diffusion_Solver::initialize(double T, double dt, int Lx, double dx){
+  /*
+  Setting up necesseary parameters. Input:
+  - T: Time span from t = 0.
+  - dt: Homogenous step size in time.
+  - Lx: The length of the system (1D) [0,Lx]
+  - dx : Homogenous step size in space
+  */
+
+  m_T = T;
+  m_dt = dt;
+  m_Lx = Lx;
+  m_dx = dx; //
+  m_dx2 = dx*dx;
+
+  // setting up spatial parameters
+  m_Nx = round(m_Lx/((double) dx)); // number of spatial iterations
+  m_x = linspace<vec>(0,Lx,m_Nx+1);  // +1 because then we can use index Nx for last element
+
+  m_Nt = round(T/((double) m_dt));              // Integer number of time iterations
+  t = linspace<vec>(0,m_Nt*m_dt,m_Nt+1);              // time mesh
+
+  // Initialize vectors (1 dimensional mesh
+  u_nn = zeros<vec>(m_Nx+1);           // time n-1, so that t -dt
+  u_n =  zeros<vec>(m_Nx+1);       // time n, so that
+  u =  zeros<vec>(m_Nx+1);             // time n+1, so that t + dt
+
+  // needing to use ghost cells i = -1,0 ... Nx+1 for implicit method?
+
+  // Index sets, only iterates over INNER points
+  Ix = linspace<vec>(1,u.n_elem-1, u.n_elem-1); //(end-start +1) to get endpoint
+  It = linspace<vec>(0,t.n_elem,t.n_elem + 1);  // time index set
 }
-// setting up initial conditions
