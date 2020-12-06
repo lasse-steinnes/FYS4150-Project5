c2a22cb93ea56c4b453d36a9abdc70f651610aad
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-12-06 14:32:48 +0100

    Fixed typo in main

--- code-and-results/main.cpp
+++ code-and-results/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[]){
   //Catch::Session().run();  // testing some numberically exact cases
   int method_solver;
   cout << "Press 1 to run explicit Euler \n";
-  cout << "Press 2 to run inplicit Euler \n";
+  cout << "Press 2 to run implicit Euler \n";
   cout << "Press 3 to run implicit Crank Nicolson \n";
   cout << "Enter number:" << " ";
   cin >> method_solver;
